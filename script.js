cdf4b943d3d5630e89f230a82d20d32a9ed889c3
sjoshi-1 <200455953@student.georgianc.on.ca>
2023-04-08 01:11:21 -0400

    reviews

--- script.js
+++ script.js
@@ -41,3 +41,26 @@ imgBtn.forEach(btn=>{
         document.querySelector('#image-slider').src = src;
     });
 });
+
+var swiper = new Swiper(".review-slider",{
+    spaceBetween:20,
+    loop:true,
+    autoplay:{
+        delay:2500,
+        disableOnInteraction:false,
+
+    },
+   breakpoints: {
+        640:{
+            slidesPerView:1,
+
+        },
+        768:{
+            slidesPerView:2,
+        },
+        1024:{
+            slidesPerView:3,
+        },
+    },
+});
+
